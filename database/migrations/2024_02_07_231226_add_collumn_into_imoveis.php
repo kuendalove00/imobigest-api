53c547c75f6f132ba45f030c4fa92a93922b0cb7
Helio <heliobras3@hotmail.com>
2024-02-08 00:28:15 +0100

    remover localixacao

--- database/migrations/2024_02_07_231226_add_collumn_into_imoveis.php
+++ database/migrations/2024_02_07_231226_add_collumn_into_imoveis.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::table('imoveis', function (Blueprint $table) {
             $table->string("slug");
             $table->text("desc")->nullable();
-            $table->string("localizacao")->nullable();
         });
     }
 
